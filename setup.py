1d8e4cda41120e6872913220f5b715b04bbae751
Prabhat Rastogi <prabhatrastogik@gmail.com>
2022-07-26 04:38:52 +0530

    Add setup.py corrections

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 DESCRIPTION = 'Zerodha Login Automation - Selenium Based'
 with open('README.md', 'r') as f:
     LONG_DESCRIPTION = f.read()
@@ -13,9 +13,11 @@ setup(
     author_email="prabhatrastogik@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     license="MIT",
-    install_requires=[],  # add any additional packages that
+    install_requires=['boto3', 'undetected_chromedriver',
+                      'selenium'],  # add any additional packages that
     keywords=['selenium login', 'zerodha', 'automate zerodha'],
     classifiers=[
         "Development Status :: 3 - Alpha",
