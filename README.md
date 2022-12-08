e5968d79ef5dc89437f52703ae3a511e2b452a3a
prabhatrastogik <prabhatrastogik@gmail.com>
2022-12-08 19:21:15 +0530

    Separated Login Wrt, package to read from dynamodb

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Zerodha Login Automated - Selenium Based; Supported by Lambda function that processes the redirect rul and stores accessk key to DynamoDB
-
+# Zerodha Login Automated; This package reads access token stored in dynamodb and provides KiteConnect / KiteTicker instances.
+# This package can only be used in conjunction with a dynamodb access_store that keeps access key stored 
 ## Context
-This code is for a python package for automated login to Zerodha. The redirect URL is not handled here - Separate Repo with a container that runs as a lambda function saves the access token to dynamoDB
+This code is for a python package for automated login to Zerodha. The redirect URL is not handled here - Separate Repos with a container that runs as a lambda function saves the access token to dynamoDB
+
+Check out zlogin-puppeteer (headless chrome based login to zerodha) [can be hosted for free in Google cloud functions]
+and zerodha-dynamodb-save-accesstoken [AWS lambda container] to save access token to dynamodb from redirect url.
 
 ## How to productionalize
 - Get Zerodha (kite.trade) account for API based access to the platform.
-- Create a '.zerodha' file in your home directory (Sample provided - check .zerodha.sample file)
-    - Needed Variables in File are Zerodha API keys/secret and login credentials (Userid, Password, PIN)
-    - One additional variable defined - ZAPI_AUTH: This can be any string that you choose. This string gets added as a query parameter in Zerodha reddirect response. The redirect response handler can test this 'api_auth' query parameter to ensure the response is from zerodha and is in response to call from this package
+- Save the API key as env variable ZAPI
 
-    The url for logging in being used is - f'https://kite.trade/connect/login?v=3&api_key={api_key}&redirect_params={quote_plus(f"api_auth={api_auth}")}' which sends the ZAPI_AUTH parameter as redirect query parameter 'api_auth'
-- Set up a dynamodb table "save_access" with partition key as 'date' (string variable) and sort key as 'datetime' (also a string)
+- Checkout zlogin-puppeteer repository that automates login process (using headless chrome)
 - Check out the zerodha-dynamodb-save-accesstoken repository to set up a lambda function to handle Zerodha redirect url (it saves the access token in the save_access table in dynamodb)
 - Install zlogin package from pypi, "pip install zlogin"
 
@@ -19,8 +19,4 @@ import zlogin
 access_token = zlogin.fetch_access_token()
 ```
 
-    When logging in - send this as a redirect parameter 'api_auth'. For this, your Zerodha login URL will be
-    f'https://kite.trade/connect/login?v=3&api_key={api_key}&redirect_params={quote_plus(f"api_auth={api_auth}")}'
-
-## This repo is a sister repo with zerodha-dynamodb-save-accesstoken tool which has a container code for lambda function that handles storage of access token to dynamodb; 
-That tool creates the redirect to the API gateway. For both to work properly, please use the same  ZAPI_AUTH environment variable value in both places
+## This repo is a sister repo with zerodha-dynamodb-save-accesstoken & zlogin-puppeteer tools which have a container code for lambda function that handles storage of access token to dynamodb and puppeteer based Google Cloud Function code to handle login process which redirects to the lambda.
--- zlogin/zlogin.py
+++ zlogin/zlogin.py
@@ -1,21 +1,13 @@
 """
-ZLOGIN Module is for auto-login for Zerodha and storage of the access token to dynamoDB. 
-This works in conjunction with store_access module which creates a Lambda container handling re-direct url from zerodha
+ZLOGIN Module is for auto-login for Zerodha through retrieval of the access token to dynamoDB.
+This works in conjunction with access storage module which creates a Lambda container handling re-direct url from zerodha
+Check the repos at zerodha-dynamodb-save-accesstoken, zlogin-puppeteer
 """
 import os
 import sys
-from time import sleep
-from urllib.parse import quote_plus
 from datetime import datetime
 from logging import getLogger, INFO, StreamHandler
-
 import boto3
-from undetected_chromedriver import Chrome
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ChromeOptions
-from pyotp import TOTP
-
 from kiteconnect import KiteConnect, KiteTicker
 
 logger = getLogger("Zlogin")
@@ -23,73 +15,18 @@ logger.addHandler(StreamHandler(sys.stdout))
 logger.setLevel(INFO)
 
 
-def add_env_vars_from_file():
-    path = os.path.expanduser('~/.zerodha')
-    with open(path, 'r') as f:
-        env_vars = dict(tuple(line.replace('\n', '').split('=')) for line
-                        in f.readlines() if not line.startswith('#'))
-    os.environ.update(env_vars)
-
-
-def get_env_vars():
-    add_env_vars_from_file()
-    return dict(
-        api_key=os.getenv('ZAPI'),
-        api_secret=os.getenv('ZSECRET'),
-        api_auth=os.getenv('ZAPI_AUTH'),
-        user_id=os.getenv('ZUSER'),
-        pw=os.getenv('ZPASS'),
-        totp_key=os.getenv('ZTOTP'),
-    )
-
-
-def get_driver():
-    options = ChromeOptions()
-    options.headless = True
-    logger.info("Driver Options Created")
-    driver = Chrome(options=options)
-    logger.info("Chrome Created")
-    return driver
-
-
-def generate_request_token(api_key, user_id, pw, totp_key, api_auth, **kwargs):
-    logger.info("Generate Function Started")
-    driver = get_driver()
-    logger.info("Driver Acquired")
-    driver.get(
-        f'https://kite.trade/connect/login?v=3&api_key={api_key}&redirect_params={quote_plus(f"api_auth={api_auth}")}')
-    login_id = WebDriverWait(driver, 10).until(
-        lambda x: x.find_element(By.XPATH, '//input[@type="text"]'))
-    login_id.send_keys(user_id)
-    logger.info("USER ID Sent")
-    pwd = WebDriverWait(driver, 10).until(
-        lambda x: x.find_element(By.XPATH, '//input[@type="password"]'))
-    pwd.send_keys(pw)
-    logger.info("Pw Sent")
-    sleep(1)
-    submit = WebDriverWait(driver, 10).until(lambda x: x.find_element(
-        By.XPATH, '//button[@type="submit"]'))
-    submit.click()
-    sleep(1)
-    logger.info("Login Btn Clicked")
-    totp = WebDriverWait(driver, 10).until(
-        lambda x: x.find_element(By.XPATH, '//form//input'))
-    totp.send_keys(TOTP(totp_key).now())
-    logger.info("TFA Sent")
-    sleep(1)
-    continue_btn = WebDriverWait(driver, 10).until(lambda x: x.find_element(
-        By.XPATH, '//button[@type="submit"]'))
-    continue_btn.click()
-    logger.info("Request Token Generated!")
+def get_env_zapi():
+    """Get env vars of the setup - basically to get the Zerodha API key"""
+    # add_env_vars_from_file()
+    return os.getenv('ZAPI')
 
 
 def fetch_access_token():
     """
-    This function fetches access token either from Dynamodb - if the token exists for today - or generates request_token
-    and passes to the Lambda function to get access_token and save to DynamoDB
-    Args: 
+    This function fetches access token either from Dynamodb - if the token exists for today - or raises Exception if no token present for today
+    Args:
         None
-    Requirements: 
+    Requirements:
         A '.zerodha' file in the home directory with values for ZAPI, ZSECRET, ZAPI_AUTH, ZUSER, ZPASS, ZTOTP
         Sample in the repo
     """
@@ -101,38 +38,22 @@ def fetch_access_token():
             'date').eq(today),
         ScanIndexForward=False
     )['Items']
-    if not date_rows:
-        logger.info("Access token for today does not exist - Generating new..")
-        generate_request_token(**get_env_vars())
-        wait_seconds = 0
-        while not date_rows:
-            date_rows = access_store.query(
-                KeyConditionExpression=boto3.dynamodb.conditions.Key(
-                    'date').eq(today),
-                ScanIndexForward=False
-            )['Items']
-            sleep(1)
-            wait_seconds += 1
-            logger.info(f'Seconds Waited: {wait_seconds}')
-            if wait_seconds > 10:
-                logger.info(
-                    f'Exiting without success in reading newly generated access token - Seconds Waited: {wait_seconds}')
-                break
     try:
         logger.info("Access Token Returned!")
         return date_rows[0]['access_token']
-    except Exception as e:
+    except Exception as exp:
         logger.error(
             'Access token not generated - Neither does one exist for today')
-        raise e
+        raise exp
 
 
-def fetch_kiteconnect_instance():
+def fetch_kiteconnect_instance(api_key=''):
+    """Returns KiteConnect instance for further analysis"""
     access_token = fetch_access_token()
-    return KiteConnect(get_env_vars()['api_key'], access_token)
+    return KiteConnect(api_key or get_env_zapi(), access_token)
 
 
-def fetch_kiteticker_instance():
+def fetch_kiteticker_instance(api_key=''):
     """
     Returns a KiteTicker Object (WebSocket) - that can be used by defining:
         kws.on_ticks = on_ticks
@@ -140,7 +61,7 @@ def fetch_kiteticker_instance():
         kws.on_close = on_close
     """
     access_token = fetch_access_token()
-    return KiteTicker(get_env_vars()['api_key'], access_token)
+    return KiteTicker(api_key or get_env_zapi(), access_token)
 
 
 if __name__ == "__main__":
